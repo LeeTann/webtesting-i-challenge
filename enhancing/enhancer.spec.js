4fe62547061f8cb1e29a86865c4481595b2a0f6a
ltann <leeroyleeroy18@gmail.com>
2019-04-08 13:00:27 -0700

    Installed dependencies. Repair function pass

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get} = require('./enhancer.js');
 // test away!
+
+test('Repair item to full durability', () => {
+    const item1 = {
+        name: 'Lambda Shield',
+        durability: 75,
+        enhancement: '[+5] Lambda Shield'
+    }
+
+    const expected = {
+        name: 'Lambda Shield',
+        durability: 100,
+        enhancement: '[+5] Lambda Shield'
+    }
+
+    expect(repair(item1.durability))
+})
