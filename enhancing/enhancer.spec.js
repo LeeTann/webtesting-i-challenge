5792630cb3b50cf874998b1e6fb6a646a66cd8a1
ltann <leeroyleeroy18@gmail.com>
2019-04-08 19:54:44 -0700

    Tested succeed function to pass

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,18 +1,40 @@
 const { succeed, fail, repair, get} = require('./enhancer.js');
 // test away!
 
-test('Repair item to full durability', () => {
-    const item1 = {
-        name: 'Lambda Shield',
-        durability: 75,
-        enhancement: '[+5] Lambda Shield'
-    }
+describe('Enhancer Testing Project', () => {
+    describe('Repair Item Testing', () => {
+        test('Repair item to full durability', () => {
+            const item1 = {
+                name: 'Lambda Shield',
+                durability: 75,
+                enhancement: 20
+            }
+        
+            const expected = {
+                name: 'Lambda Shield',
+                durability: 100,
+                enhancement: 20
+            }
+        
+            expect(repair(item1).durability)
+        })
+    })
 
-    const expected = {
-        name: 'Lambda Shield',
-        durability: 100,
-        enhancement: '[+5] Lambda Shield'
-    }
+    describe('Enhance Item Testing', () => {
+        test('Enhance item by 1', () => {
+            const item2 = {
+                name: 'Lambda Sword',
+                durability: 80,
+                enhancement: 10,
+            }
 
-    expect(repair(item1.durability))
+            const expected = {
+                name: 'Lambda Sword',
+                durability: 80,
+                enhancement: 11,
+            }
+
+            expect(succeed(item2).enhancement).toBe(expected.enhancement)
+        })
+    })
 })
